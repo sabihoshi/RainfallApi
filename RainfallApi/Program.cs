98624fed0c88bd8920c72fa595cc5e946c242f6c
sabihoshi <sabihoshi.dev@gmail.com>
2024-04-12 17:43:50 +0800

    feat: Add SwaggerDoc configuration
    - Bump Swashbuckle.AspNetcore to 6.5.0
    - Configure port to 3000

--- RainfallApi/Program.cs
+++ RainfallApi/Program.cs
@@ -1,3 +1,6 @@
+using System.Reflection;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +8,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version     = "v1.0",
+        Title       = "Rainfall API",
+        Description = "An API which provides rainfall reading data"
+    });
+
+    var xml = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xml));
+});
 
 var app = builder.Build();
 
