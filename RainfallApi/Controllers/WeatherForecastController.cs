99a53d057ca8d38fe530ae25a2689e4433a8e3e8
sabihoshi <sabihoshi.dev@gmail.com>
2024-04-12 17:44:04 +0800

    feat: Add .editorconfig

--- RainfallApi/Controllers/WeatherForecastController.cs
+++ RainfallApi/Controllers/WeatherForecastController.cs
@@ -6,27 +6,21 @@ namespace RainfallApi.Controllers;
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
+    private static readonly string[] Summaries =
     {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
-    {
-        _logger = logger;
-    }
+    public WeatherForecastController(ILogger<WeatherForecastController> logger) { _logger = logger; }
 
     [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
-    {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+    public IEnumerable<WeatherForecast> Get() => Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Date         = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
             TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            Summary      = Summaries[Random.Shared.Next(Summaries.Length)]
         })
-        .ToArray();
-    }
+       .ToArray();
 }
